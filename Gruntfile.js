dac83ab16cbb6352ecba9f2c7307e914b8aaaa88
rummik <r@9k1.us>
2014-03-18 17:35:24 -0400

    Pull info from bower.json instead of package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,13 @@ module.exports = function(grunt) {
 	'use strict';
 
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON('bower.json'),
 
 		banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
 		        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
 		        '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-		        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-		        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+		        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.authors %>;' +
+		        ' Licensed <%= pkg.license %> */\n',
 
 		watch: {
 			packagejson: {
