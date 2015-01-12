fe3708beeeabb50bc40eff718c873873c6200b5f
rummik <r@9k1.us>
2015-01-12 16:19:33 -0500

    Use gulp-load-tasks

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,4 @@ nconf.argv().env().file('config/default.json');
 gulp.plugin = require('gulp-load-plugins')();
 gulp.config = nconf.get.bind(nconf);
 
-fs.readdirSync('tasks').forEach(function(file) {
-	if (file[0] === '_' || file.slice(-3) !== '.js') {
-		return;
-	}
-
-	var name = file.slice(0, -3);
-	var task = require(process.cwd() + '/tasks/' + file);
-
-	gulp.task.apply(gulp, [name].concat(task));
-});
+require('gulp-load-tasks')('tasks');
